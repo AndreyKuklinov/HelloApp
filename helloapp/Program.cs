1e90d760b59a2f50c0e5f6dcc23f8e909f8d2657
black <black@example.com>
2021-11-03 23:51:54 +0500

    Исправления

--- helloapp/Program.cs
+++ helloapp/Program.cs
@@ -7,6 +7,8 @@ namespace helloapp
         static void Main(string[] args)
         {
             Console.WriteLine("hello everyone");
+            GreetWhite();
+            GreetBlack();
         }
 
         static void GreetWhite()
